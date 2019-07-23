a66d507b004c8a3363e923895e6102bd0934df57
r.varmazyari <the.alxan@gmail.com>
2019-07-23 16:39:12 +0430

    Intial copy of TlObjects.

---
+++ src/main/java/io/tlobjects/tl/core/TLGzipObject.java
@@ -0,0 +1,75 @@
+package io.tlobjects.tl.core;
+
+import io.tlobjects.tl.StreamUtils;
+import io.tlobjects.tl.TLContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import static io.tlobjects.tl.TLObjectUtils.computeTLBytesSerializedSize;
+
+/**
+ * @author Yannick Badoual yann.badoual@gmail.com
+ * @see <a href="http://github.com/badoualy/kotlogram">http://github.com/badoualy/kotlogram</a>
+ */
+public class TLGzipObject extends TLObject {
+
+    public static final int CONSTRUCTOR_ID = 0x3072cfa1;
+
+    private byte[] packedData;
+
+    public TLGzipObject() {
+
+    }
+
+    public TLGzipObject(byte[] packedData) {
+        this.packedData = packedData;
+    }
+
+
+    @Override
+    public void serializeBody(OutputStream stream) throws IOException {
+        StreamUtils.writeTLBytes(packedData, stream);
+    }
+
+    @Override
+    public void deserializeBody(InputStream stream, TLContext context) throws IOException {
+        packedData = StreamUtils.readTLBytes(stream);
+    }
+
+    @Override
+    public int computeSerializedSize() {
+        return CONSTRUCTOR_ID + computeTLBytesSerializedSize(packedData.length);
+    }
+
+    @Override
+    public String toString() {
+        return "gzip_packed#3072cfa1";
+    }
+
+    @Override
+    public int getConstructorId() {
+        return CONSTRUCTOR_ID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof TLGzipObject))
+            return false;
+        if (this == obj)
+            return true;
+
+        TLGzipObject o = (TLGzipObject) obj;
+        return Arrays.equals(packedData, o.packedData);
+    }
+
+    public byte[] getPackedData() {
+        return packedData;
+    }
+
+    public void setPackedData(byte[] packedData) {
+        this.packedData = packedData;
+    }
+}
---
+++ src/main/java/io/tlobjects/tl/exception/DeserializationException.java
@@ -0,0 +1,11 @@
+package io.tlobjects.tl.exception;
+
+import java.io.IOException;
+
+public class DeserializationException extends IOException {
+
+    public DeserializationException(String s) {
+        super(s);
+    }
+
+}
