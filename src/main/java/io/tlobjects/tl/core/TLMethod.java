53a53632744b4d9a7779977475227df05a06791f
r.varmazyari <the.alxan@gmail.com>
2019-07-23 16:39:12 +0430

    Intial copy of TlObjects.

---
+++ src/main/java/io/tlobjects/tl/core/TLMethod.java
@@ -0,0 +1,35 @@
+package io.tlobjects.tl.core;
+
+
+import io.tlobjects.tl.TLContext;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Basic object for RPC method.
+ * It contains special method for deserializing result of RPC method call.
+ *
+ * @param <T> return type of method
+ * @author Yannick Badoual yann.badoual@gmail.com
+ * @see <a href="http://github.com/badoualy/kotlogram">http://github.com/badoualy/kotlogram</a>
+ */
+public abstract class TLMethod<T extends TLObject> extends TLObject {
+
+    private transient T response;
+
+    public T deserializeResponse(byte[] data, TLContext context) throws IOException {
+        return response = deserializeResponse(new ByteArrayInputStream(data), context);
+    }
+
+    public abstract T deserializeResponse(InputStream stream, TLContext context) throws IOException;
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
+    }
+}
---
+++ src/main/java/io/tlobjects/tl/exception/UnsupportedConstructorException.java
@@ -0,0 +1,8 @@
+package io.tlobjects.tl.exception;
+
+public class UnsupportedConstructorException extends DeserializationException {
+
+    public UnsupportedConstructorException(int constructorId) {
+        super("Unsupported constructor: #" + Integer.toHexString(constructorId));
+    }
+}
