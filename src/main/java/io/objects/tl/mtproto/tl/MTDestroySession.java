be3d9a7cc2160f11f2f6763c3fe8c7be63bf8047
badiezadegan <badiezadegan@gmail.com>
2019-09-11 19:18:42 +0430

    Add sessionId to MTDestroySession.

--- src/main/java/io/objects/tl/mtproto/tl/MTDestroySession.java
+++ src/main/java/io/objects/tl/mtproto/tl/MTDestroySession.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import static io.objects.tl.StreamUtils.readTLObject;
+import static io.objects.tl.StreamUtils.*;
 import static io.objects.tl.TLObjectUtils.SIZE_CONSTRUCTOR_ID;
-import static io.objects.tl.TLObjectUtils.SIZE_INT32;
+import static io.objects.tl.TLObjectUtils.SIZE_INT64;
 
 /**
  * This class is updated by MOHoLiaghat: V85a
@@ -18,24 +18,27 @@ import static io.objects.tl.TLObjectUtils.SIZE_INT32;
 public class MTDestroySession extends TLMethod<MTAbsDestroySessionResponse> {
     public static final int CONSTRUCTOR_ID = 0xe7512126;
 
+    protected long sessionId;
+
     public MTDestroySession() {
     }
 
     public void serializeBody(OutputStream stream) throws IOException {
+        writeLong(sessionId, stream);
     }
 
     @Override
     @SuppressWarnings({"unchecked", "SimplifiableConditionalExpression"})
     public void deserializeBody(InputStream stream, TLContext context) throws IOException {
+        sessionId = readLong(stream);
     }
 
     @Override
     public int computeSerializedSize() {
         int size = SIZE_CONSTRUCTOR_ID;
-        size += SIZE_INT32;
+        size += SIZE_INT64;
         return size;
     }
-
 
     @Override
     public int getConstructorId() {
@@ -52,5 +55,13 @@ public class MTDestroySession extends TLMethod<MTAbsDestroySessionResponse> {
             throw new IOException("Incorrect response type, expected " + getClass().getCanonicalName() + ", found " + response.getClass().getCanonicalName());
         }
         return (MTAbsDestroySessionResponse) response;
+    }
+
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(long sessionId) {
+        this.sessionId = sessionId;
     }
 }
