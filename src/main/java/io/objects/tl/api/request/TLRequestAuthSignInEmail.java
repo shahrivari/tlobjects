1ad8148a25d8f7ae60e13da5f8752bc463459e90
Mohammad Hossein Liaghat <mohamadliaghat>
2019-12-19 02:33:56 +0330

    Add new TL for sign up with email.

---
+++ src/main/java/io/objects/tl/api/request/TLRequestAuthSignInEmail.java
@@ -0,0 +1,79 @@
+package io.objects.tl.api.request;
+
+import io.objects.tl.TLContext;
+import io.objects.tl.core.TLBool;
+import io.objects.tl.core.TLMethod;
+import io.objects.tl.core.TLObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static io.objects.tl.StreamUtils.*;
+import static io.objects.tl.TLObjectUtils.SIZE_CONSTRUCTOR_ID;
+import static io.objects.tl.TLObjectUtils.computeTLStringSerializedSize;
+
+public class TLRequestAuthSignInEmail extends TLMethod<TLBool> {
+    public static final int CONSTRUCTOR_ID = 0xbcd43581;
+
+    private final String _constructor = "auth.signInEmail#bcd43581";
+
+    protected String email;
+
+    public TLRequestAuthSignInEmail() {
+    }
+
+    public TLRequestAuthSignInEmail(String phoneNumber, String phoneCodeHash, String phoneCode, String email) {
+        this.email = email;
+    }
+
+    @Override
+    @SuppressWarnings({"unchecked", "SimplifiableConditionalExpression"})
+    public TLBool deserializeResponse(InputStream stream, TLContext context) throws
+            IOException {
+        final TLObject response = readTLObject(stream, context);
+        if (response == null) {
+            throw new IOException("Unable to parse response");
+        }
+        if (!(response instanceof TLBool)) {
+            throw new IOException("Incorrect response type, expected " + getClass().getCanonicalName() + ", found " + response.getClass().getCanonicalName());
+        }
+        return (TLBool) response;
+    }
+
+    @Override
+    public void serializeBody(OutputStream stream) throws IOException {
+        writeString(email, stream);
+    }
+
+    @Override
+    @SuppressWarnings({"unchecked", "SimplifiableConditionalExpression"})
+    public void deserializeBody(InputStream stream, TLContext context) throws IOException {
+        email = readTLString(stream);
+    }
+
+    @Override
+    public int computeSerializedSize() {
+        int size = SIZE_CONSTRUCTOR_ID;
+        size += computeTLStringSerializedSize(email);
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return _constructor;
+    }
+
+    @Override
+    public int getConstructorId() {
+        return CONSTRUCTOR_ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
